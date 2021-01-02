a6f84c57e9c0d3a929e4615c8a7d6746033b0e68
Alpha Barry <rickbarry133@gmail.com>
2021-01-02 11:44:25 -0700

    initial build of my tip calaculator app

---
+++ android/app/src/main/kotlin/com/example/tipCalculatorApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tipCalculatorApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/util/hexcolor.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HexColor extends Color {
+  static int _getColorsFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorsFromHex(hexColor));
+}
+
+//how to use:
+
+/*
+  Color color1 = HexColor("b74093")
+  Color color2 = HexColor("#b74093")
+  Color color3 = HexColor("#88b74093") // if you wish to use ARGB form
+ */
